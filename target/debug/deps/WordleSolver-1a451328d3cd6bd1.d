f623eb85a5b556589ea03e1484c96fe6759e4f1b
Natnael Yohannes <natnael.m.yohannes@gmail.com>
2026-07-01 20:06:58 -0400

    rust added target folder, also renamed words folder

---
+++ target/debug/deps/WordleSolver-1a451328d3cd6bd1.d
@@ -0,0 +1,5 @@
+C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\WordleSolver-1a451328d3cd6bd1.d: src\main.rs
+
+C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\libWordleSolver-1a451328d3cd6bd1.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/WordleSolver-a0d2a06e35453886.d
@@ -0,0 +1,5 @@
+C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\WordleSolver-a0d2a06e35453886.d: src\main.rs
+
+C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\libWordleSolver-a0d2a06e35453886.rmeta: src\main.rs
+
+src\main.rs:
