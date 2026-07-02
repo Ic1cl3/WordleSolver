e0444d2eb0f94e3252d718343c793816fe3ec428
Natnael Yohannes <natnael.m.yohannes@gmail.com>
2026-07-01 20:16:12 -0400

    fixed target

--- target/debug/deps/WordleSolver-a0d2a06e35453886.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\WordleSolver-a0d2a06e35453886.d: src\main.rs
-
-C:\Users\Precision 5750\Downloads\WordleSolver\target\debug\deps\libWordleSolver-a0d2a06e35453886.rmeta: src\main.rs
-
-src\main.rs:
