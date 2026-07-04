9839051c8dda9a14317d5ddbd37ca298ce298f13
Natnael Yohannes <natnael.m.yohannes@gmail.com>
2026-07-04 17:12:13 -0400

    Added guess scoring function

--- wordGuesser.py
+++ wordGuesser.py
@@ -19,3 +19,40 @@ def filterAnswers(answers : list[str], information : dict[str, infos.info]) -> l
                     possibleAnswers.remove(answer)
     
     return possibleAnswers
+
+def getGuessImperfection(guess : str, answers : list[str], information : dict[str, infos.info]) -> int:
+    """
+    Returns an imperfection score for the guess based on the information known about the letters.
+    """
+    score : float = 0.0
+    poolSize : float = len(answers)
+    possibleResults : int = 3 ** len(guess)
+    possibleInfos : list[dict[str, infos.info]] = [information.copy() for i in range(possibleResults)]
+    possibleMasks : list[str] = getBaseThreeList(len(guess))
+    for maskI in range(len(possibleMasks)):
+        mask = possibleMasks[maskI]
+        for charI in range(len(mask)):
+            char = guess[charI]
+            if mask[charI] == "0":
+                possibleInfos[maskI][char] = infos.infofull(-1)
+            elif mask[charI] == "1":
+                possibleInfos[maskI][char] = infos.infopartial()
+                possibleInfos[maskI][char].markIndex(charI)
+            elif mask[charI] == "2":
+                possibleInfos[maskI][char] = infos.infofull(charI)
+    for possibleInfo in possibleInfos:
+        possibleAnswers : list[str] = filterAnswers(answers, possibleInfo)
+        score += abs((1/possibleResults) - (len(possibleAnswers) / poolSize))
+    return score
+
+def getBaseThreeList(length : int) -> list[str]:
+    """
+    Returns a list of all possible base 3 masks for a given length.
+    """
+    masks : list[str] = []
+    for i in range(3 ** length):
+        mask : str = ""
+        for j in range(length):
+            mask += str((i // (3 ** j)) % 3)
+        masks.append(mask)
+    return masks
